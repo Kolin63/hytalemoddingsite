e0580e624d4ea93a8f724862833bb1da57dc67bf
honkling <untitledgeese@gmail.com>
2025-11-19 22:16:19 -0600

    fix: define repo name in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -6,8 +6,13 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex(), ],
-	kit: { adapter: adapter() },
+	preprocess: [vitePreprocess(), mdsvex()],
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: dev ? "" : "/site"
+		}
+	},
 	extensions: [".svelte", ".svx"]
 };
 
