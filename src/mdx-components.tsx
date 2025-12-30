3f31164dc62a1e94287bb70804ebe26ead9fd7a0
ItsNeil17 <neil@willofsteel.me>
2025-12-31 05:26:01 +0530

    revert: image zoom
    Signed-off-by: ItsNeil17 <neil@willofsteel.me>

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,12 +1,10 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 import { Callout } from "@/components/mdx/Callout";
-import { ImageZoom } from 'fumadocs-ui/components/image-zoom';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
-    img: (props) => <ImageZoom {...(props as any)} />,
     ...components,
     Callout,
   };
